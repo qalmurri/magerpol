fb071b06536bc64ffead8dd7201febde803cac79
qalmurri <blog.qalmurri@gmail.com>
2026-03-10 22:04:26 +0700

    Restruktur

--- Client/cpp/register_types.cpp
+++ Client/cpp/register_types.cpp
@@ -3,22 +3,6 @@
 #include <godot_cpp/godot.hpp>
 #include <godot_cpp/core/class_db.hpp>
 
-#include "Core/MapManager.h"
-#include "Core/World.h"
-#include "Core/CoordinateSystem.h"
-#include "Core/GridSystem.h"
-#include "Core/TileSystem.h"
-
-#include "Player/Movement.h"
-#include "Player/Rotation.h"
-#include "Player/InputHandler.h"
-#include "Player/Players.h"
-
-#include "Gameplay/CombatManager.h"
-#include "Gameplay/DropManager.h"
-#include "Gameplay/EntityManager.h"
-#include "Gameplay/TileEventManager.h"
-
 using namespace godot;
 
 void initialize_module(ModuleInitializationLevel p_level) {
@@ -26,21 +10,7 @@ void initialize_module(ModuleInitializationLevel p_level) {
         return;
     }
 
-    ClassDB::register_class<MapManager>();
-    ClassDB::register_class<World>();
-    ClassDB::register_class<CoordinateSystem>();
-    ClassDB::register_class<GridSystem>();
-    ClassDB::register_class<TileSystem>();
-
-    ClassDB::register_class<Movement>();
-    ClassDB::register_class<Rotation>();
-    ClassDB::register_class<InputHandler>();
-    ClassDB::register_class<Players>();
-
-    ClassDB::register_class<CombatManager>();
-    ClassDB::register_class<DropManager>();
-    ClassDB::register_class<EntityManager>();
-    ClassDB::register_class<TileEventManager>();
+    // ClassDB::register_class<MapManager>();
 }
 
 void uninitialize_module(ModuleInitializationLevel p_level) {
